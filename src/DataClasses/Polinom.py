d861cbc534f441d15d36ce2669d1679bc0be2731
Deineko Vladimir <vladimir.deyneko@yandex.com>
2026-04-19 11:37:26 +0300

    Release data class Polynom

--- src/DataClasses/Polinom.py
+++ src/DataClasses/Polinom.py
@@ -1,2 +1,56 @@
+from RationalNumber import RationalNumber
+
+
 class Polinom:
-    pass
+    """
+    Реализация многочлена
+    Аргументы:
+        sign (int): 1 - минус, 0 - плюс.
+        deg (int): степень многочлена
+        nomer (str): числитель коэффициента
+        denomer (str): знаменатель коэффициента
+    """
+    def __init__(self, sign=0, deg=0, nomer="", denomer=""):
+        self.deg = deg
+        self.coefs = {
+            deg: RationalNumber(sign, nomer, denomer)
+                      }
+        for i in range(deg - 1, -1, -1):
+            self.coefs[i] = RationalNumber()
+
+    def change_deg(self, new_deg):
+        if self.deg > new_deg:
+            for i in range(self.deg + 1, new_deg, -1):
+                self.coefs.pop(i)
+        else:
+            for i in range(self.deg + 1, new_deg + 1):
+                self.coefs[i] = RationalNumber()
+        self.deg = new_deg
+
+    def change_coef(self, sign=0, deg=0, nomer="", denomer=""):
+        if deg > self.deg:
+            self.change_deg(deg)
+
+        self.coefs[deg] = RationalNumber(sign, nomer, denomer)
+
+    def __str__(self):
+        string = ""
+        for i in range(self.deg + 1):
+            coef = self.coefs[i]
+            formatted_coef = str(coef).replace('\n', '\n\t\t')
+            string += f"\tСтепень x^{i}:\n\t\t{formatted_coef}\n"
+
+        return (
+            f"Polinom: {{\n\t"
+            f"Степень многочлена {self.deg}\n"
+            f"{string}"
+            f"}}"
+        )
+
+
+if __name__ == "__main__":
+    p = Polinom(1, 3, "23457", "23712")
+    p.change_coef(1, 3, "123", "123")
+    p.change_coef(1, 5, "234", "54")
+    p.change_coef(1, 2, "543", "23")
+    print(p)
