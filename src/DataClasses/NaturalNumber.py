6683a0f54c3ec0a4647a5ccc88d4b3b7bd6e7525
Kilina Sofya <sophakilina805@mail.ru>
2026-04-28 03:52:58 +0300

    Вычисление первой цифры деления большего натурального на меньшее, домноженное на 10^k,где k - номер позиции этой цифры (номер считается с нуля

--- src/DataClasses/NaturalNumber.py
+++ src/DataClasses/NaturalNumber.py
@@ -88,6 +88,71 @@ class NaturalNumber:
 
         return ''.join(map(str, reversed(num)))
 
+    # Выполнила Килина Софья 5381
+    def div_nn_dk(self, number):
+        """
+        Возвращает кортеж (count, k), где count – первая цифра частного,
+        k – номер позиции этой цифры (степень 10^k), такие что:
+        count * number * 10^k <= self < (count+1) * number * 10^k
+        """
+        cmp = self.com_nn_d(self.digits, number.digits)
+        if cmp == 0:
+            return 1, 0
+        if cmp == 1:
+            b = NaturalNumber()
+            b.digits = number.digits[:]
+            b.n = number.n
+            s = NaturalNumber()
+            s.digits = self.digits[:]
+            s.n = self.n
+        else:
+            b = NaturalNumber()
+            b.digits = self.digits[:]
+            b.n = self.n
+            s = NaturalNumber()
+            s.digits = number.digits[:]
+            s.n = number.n
+        k = 0
+        bs = NaturalNumber()
+        bs.digits = s.digits[:]
+        bs.n = s.n
+        n3 = NaturalNumber()
+        n3.digits = s.digits[:]
+        n3.n = s.n
+        while self.com_nn_d(b.digits, n3.digits) == 2:
+            k += 1
+            n3.digits = bs.digits[:]
+            n3.n = bs.n
+            n3.mul_nk_n(k)
+        k -= 1
+        s = NaturalNumber()
+        s.digits = bs.digits[:]
+        s.n = bs.n
+        n3 = NaturalNumber()
+        n3.digits = s.digits[:]
+        n3.n = s.n
+        n3.mul_nk_n(k)
+        s = n3
+
+        count = 1
+        tm = NaturalNumber()
+        tm.digits = s.digits[:]
+        tm.n = s.n
+        n3 = NaturalNumber()
+        n3.digits = s.digits[:]
+        n3.n = s.n
+        while self.com_nn_d(b.digits, n3.digits) == 2:
+            count += 1
+            n3.digits = tm.digits[:]
+            n3.n = tm.n
+            n3.mul_nd_n(count)
+        if self.com_nn_d(b.digits, n3.digits) != 0:
+            count -= 1
+        if count == 10:
+            count = 1
+            k += 1
+        return count, k
+
 
 if __name__ == "__main__":
     nn = NaturalNumber(input())
